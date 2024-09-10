686ae4b8e7046a56915f10c1373e9256dc8e54dc
SeoYeKyung <seodbsk78@gmail.com>
2024-09-10 16:57:18 +0900

    fix 학습한 내용

--- src/main/java/hello/hello_spring/SpringConfig.java
+++ src/main/java/hello/hello_spring/SpringConfig.java
@@ -1,11 +1,9 @@
 package hello.hello_spring;
 
 
-import hello.hello_spring.repository.JdbcMemberRepository;
-import hello.hello_spring.repository.JdbcTemplateMemberRepository;
-import hello.hello_spring.repository.MemberRepository;
-import hello.hello_spring.repository.MemoryMemberRepository;
+import hello.hello_spring.repository.*;
 import hello.hello_spring.service.MemberService;
+import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,14 +13,19 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
+//    private DataSource dataSource;
+//    //이렇게 만들어두면 스프링에서도 자체적으로 빈을 만들어줌.
 
-    private DataSource dataSource;
-    //이렇게 만들어두면 스프링에서도 자체적으로 빈을 만들어줌.
+    private EntityManager em;
 
-    @Autowired
-    public SpringConfig(DataSource dataSource){
-        this.dataSource=dataSource;
+    public SpringConfig(EntityManager em) {
+        this.em = em;
     }
+
+//    @Autowired
+//    public SpringConfig(DataSource dataSource){
+//        this.dataSource=dataSource;
+//    }
 
     @Bean
     public MemberService memberService(){
@@ -35,6 +38,7 @@ public class SpringConfig {
     public MemberRepository memberRepository(){
         //return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
--- src/main/java/hello/hello_spring/service/MemberServiceIntegrationTest.java
+++ src/main/java/hello/hello_spring/service/MemberServiceIntegrationTest.java
@@ -3,14 +3,14 @@ package hello.hello_spring.service;
 
 import hello.hello_spring.domain.Member;
 import hello.hello_spring.repository.MemberRepository;
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-//import static org.assertj.core.api.Assertions.*;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 
 // 이부분 자꾸 Test에서 오류가 발생함.
 //보통 테스트할 때 테스트용 DB를 따로 구축하고 테스트를 한다.
@@ -25,26 +25,32 @@ class MemberServiceIntegrationTest {
     //아레의 코드는 실행 후 로백도 가능하다.
     @Test
     public void 회원가입() throws Exception {
-        //Given
+        // Given
         Member member = new Member();
         member.setName("hello");
-        //When
+
+        // When
         Long saveId = memberService.join(member);
-        //Then
-        Member findMember = memberRepository.findById(saveId).get();
-        assertEquals(member.getName(), findMember.getName());
+
+        // Then
+        Member findMember = memberRepository.findById(saveId).orElse(null);
+        assertThat(findMember).isNotNull();
+        assertThat(findMember.getName()).isEqualTo(member.getName());
     }
     @Test
     public void 중복_회원_예외() throws Exception {
-        //Given
+        // Given
         Member member1 = new Member();
         member1.setName("spring");
+
         Member member2 = new Member();
         member2.setName("spring");
-        //When
+
+        // When
         memberService.join(member1);
-        IllegalStateException e = assertThrows(IllegalStateException.class,
-                () -> memberService.join(member2));//예외가 발생해야 한다.
-        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+
+        // Then
+        IllegalStateException exception = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        assertThat(exception.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
     }
 }
